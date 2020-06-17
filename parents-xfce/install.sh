3edcce9ed6150dde6ae593e8ec2e2305cbea583a
simondehaut <66692121+simondehaut@users.noreply.github.com>
2020-06-17 08:01:24 +0200

    Update install.sh

--- parents-xfce/install.sh
+++ parents-xfce/install.sh
@@ -14,11 +14,62 @@ echo "deb-src http://deb.debian.org/debian/ buster-updates main non-free" >> /et
 apt update
 apt upgrade -y
 
-#pci wifi card driver
-apt install firmware-iwlwifi -y
+#extend path env var
+echo "export PATH=/usr/local/sbin:/usr/sbin:/sbin:$PATH" >> ~/.bashrc
+
+#set grub timeout to 0
+sed -i -r 's/.*GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/g' /etc/default/grub
+/usr/sbin/update-grub
+
+#install: DE
+apt install xfce4 -y
+
+#install: utils
+apt install wpasupplicant -y
+apt install wget -y
+apt install xfce4-terminal -y
+apt install htop -y
+apt install hplip hplip-gui -y
+apt install mousepad -y
+apt install atril -y
+apt install xarchiver -y
+apt install ristretto -y
+apt install flatpak
+
+#install: multimedia
+apt install firefox-esr firefox-esr-l10n-fr -y
+apt install gimp -y
+apt install kolourpaint -yclear
+apt install vlc vlc-l10n -y
+
+#install: google earth pro
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/earth/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+apt update 
+apt install google-earth-pro-stable -y
+
+#libre office
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub org.libreoffice.LibreOffice -y
+
+#autostart mod : hplip
+if [ ! -f /etc/xdg/autostart/hplip-systray.desktop ]; then
+	mkdir /etc/xdg/autostart/old
+	mv /etc/xdg/autostart/hplip-systray.desktop /etc/xdg/autostart/old/hplip-systray.desktop
+fi
+#if [ ! -f /home/dehaut/autostart/hplip-systray.desktop ]; then
+#	mkdir /home/dehaut/autostart/old
+#	mv /home/dehaut/autostart/hplip-systray.desktop /home/dehaut/autostart/old/hplip-systray.desktop
+#fi
+
+#folder struct
+mkdir /home/dehaut/doc
+mkdir /home/dehaut/doc/scan
+mkdir /home/dehaut/doc/images
+mkdir /home/dehaut/doc/téléchargements
 
 #copy files from git to dehaut home folder and init
-cp -R /home/dehaut/debian-preparator/parents-xfce/home/* /home/dehaut/
+cp -R /home/dehaut/debian-preparator/parents-xfce/home/{.,}* /home/dehaut/
 chmod +x /home/dehaut/.simon/scan_gray.sh
 chmod +x /home/dehaut/.simon/scan_color.sh
 chmod +x /home/dehaut/Bureau/Writer
@@ -32,10 +83,14 @@ chmod +x /home/dehaut/Bureau/Firefox
 chmod +x /home/dehaut/Bureau/dehaut.desktop
 chmod +x /home/dehaut/.config/autostart/simon_startup_script.sh
 
+#pci wifi card driver
+apt install firmware-iwlwifi -y
+
 #touch wpa supplicant config
 touch /etc/wpa_supplicant/wpa_supplicant.conf
 
 #make conf
+echo "wifi pwd for freebox_YSFVBB_dehaut :"
 wpa_passphrase freebox_YSFVBB_dehaut >> /etc/wpa_supplicant/wpa_supplicant.conf
 
 #delete clear pswd
@@ -56,56 +111,3 @@ chmod 0600 /etc/network/interfaces
 
 #clear var
 thePsk = NULL
-
-#extend path env var
-echo "export PATH=/usr/local/sbin:/usr/sbin:/sbin:$PATH" >> ~/.bashrc
-
-#set grub timeout to 0
-sed -i -r 's/.*GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/g' /etc/default/grub
-/usr/sbin/update-grub
-
-#install: DE
-apt install xfce4 -y
-
-#install: utils
-apt install wget -y
-apt install xfce4-terminal -y
-apt install htop -y
-apt install hplip hplip-gui -y
-apt install mousepad -y
-apt install atril -y
-apt install xarchiver -y
-apt install ristretto -y
-apt install flatpak
-
-#install: multimedia
-apt install firefox-esr firefox-esr-l10n-fr -y
-apt install gimp -y
-apt install kolourpaint -yclear
-apt install vlc vlc-l10n -y
-
-#install: google earth pro
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/earth/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-apt update 
-apt install google-earth-pro-stable -y
-
-#libre office
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub org.libreoffice.LibreOffice -y
-
-#autostart mod : hplip
-if [ ! -f /etc/xdg/autostart/hplip-systray.desktop ]; then
-	mkdir /etc/xdg/autostart/old
-	mv /etc/xdg/autostart/hplip-systray.desktop /etc/xdg/autostart/old/hplip-systray.desktop
-fi
-if [ ! -f /home/dehaut/autostart/hplip-systray.desktop ]; then
-	mkdir /home/dehaut/autostart/old
-	mv /home/dehaut/autostart/hplip-systray.desktop /home/dehaut/autostart/old/hplip-systray.desktop
-fi
-
-#folder struct
-mkdir /home/dehaut/doc
-mkdir /home/dehaut/doc/scan
-mkdir /home/dehaut/doc/images
-mkdir /home/dehaut/doc/téléchargements
