0fb80a273ae1efcff811ab0f5daf49ce6aeba0d9
simondehaut <nomis@outlook.com>
2020-06-17 03:02:32 +0200

    xfce parents preparator first commit : fix

---
+++ parents-xfce/README.md
@@ -0,0 +1,7 @@
+# debian preparator for parents : xfce
+install:
+- debian net install
+- clone this git
+- cp /home/* in home folder
+- run /install.sh
+- rm cloned git
---
+++ parents-xfce/home/.simon/scan_color.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ ! -d "~/doc/" ] 
+then
+    mkdir ~/doc
+fi
+
+if [ ! -d "~/doc/scan" ] 
+then
+    mkdir ~/doc/scan
+fi
+
+cd ~/doc/scan
+
+hp-scan --device=hpaio:/usb/Deskjet_3520_series?serial=CN27B1275D05SY --resolution=300 --mode=color --compression=jpeg
